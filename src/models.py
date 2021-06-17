a2c1607e45e3e2af82709cfc7771b41aab2ca432
vaggy <sarvagya6@gmail.com>
2021-06-18 02:19:12 +0530

    Update models.py

--- src/models.py
+++ src/models.py
@@ -1,62 +1,53 @@
-import datetime
-from . import db, bcrypt
+class User(db.Model):
 
-class UserModel(db.Model):
+    __tablename__ = 'users'
 
-  __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.Integer, unique=True)
+    first_name = db.Column(db.String(32))
+    last_name = db.Column(db.String(32))
+    username = db.Column(db.String(128), nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), unique=True, nullable=False)
+    email_verified = db.Column(db.Boolean, default=False)
+    is_staff = db.Column(db.Boolean, default=False)
+    is_admin = db.Column(db.Boolean, default=False)
+    created_at = db.Column(db.DateTime)
+    modified_at = db.Column(db.DateTime)
+    blogposts = db.relationship('BlogpostModel', backref='users', lazy=True)
 
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(128), nullable=False)
-  email = db.Column(db.String(128), unique=True, nullable=False)
-  password = db.Column(db.String(128), nullable=False)
-  created_at = db.Column(db.DateTime)
-  modified_at = db.Column(db.DateTime)
-  blogposts = db.relationship('BlogpostModel', backref='users', lazy=True)
+class UserSchema(Schema):
 
-  # class constructor
-  def __init__(self, data):
-    """
-    Class constructor
-    """
-    self.name = data.get('name')
-    self.email = data.get('email')
-    self.password = self.__generate_hash(data.get('password'))
-    self.created_at = datetime.datetime.utcnow()
-    self.modified_at = datetime.datetime.utcnow()
+    id = fields.Int(dump_only=True)
+    public_id = fields.Int(dump_only=True)
+    first_name = fields.Str(required=True)
+    last_name = fields.Str(required=True)
+    username = fields.Str(required=True)
+    email = fields.Email(required=True)
+    email_verified = fields.Bool(required=True)
+    is_staff = fields.Bool(required=True)
+    is_admin = fields.Bool(required =True)
+    password = fields.Str(required=True, load_only=True)
+    created_at = fields.DateTime(dump_only=True)
+    modified_at = fields.DateTime(dump_only=True)
+    blogposts = fields.Nested(many=True)
 
-  def save(self):
-    db.session.add(self)
-    db.session.commit()
+class BlogpostModel(db.Model):
 
-  def update(self, data):
-    for key, item in data.items():
-      if key == 'password':
-        self.password = self.__generate_hash(value)
-      setattr(self, key, item)
-    self.modified_at = datetime.datetime.utcnow()
-    db.session.commit()
+    __tablename__ = 'blogposts'
 
-  def delete(self):
-    db.session.delete(self)
-    db.session.commit()
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128), nullable=False)
+    contents = db.Column(db.Text, nullable=False)
+    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    created_at = db.Column(db.DateTime)
+    modified_at = db.Column(db.DateTime)
 
-  @staticmethod
-  def get_all_users():
-    return UserModel.query.all()
+class BlogpostSchema(Schema):
 
-  @staticmethod
-  def get_one_user(id):
-    return UserModel.query.get(id)
-  
-  @staticmethod
-  def get_user_by_email(value):
-    return UserModel.query.filter_by(email=value).first()
-
-  def __generate_hash(self, password):
-    return bcrypt.generate_password_hash(password, rounds=10).decode("utf-8")
-  
-  def check_hash(self, password):
-    return bcrypt.check_password_hash(self.password, password)
-  
-  def __repr(self):
-    return '<id {}>'.format(self.id)
+    id = fields.Int(dump_only=True)
+    title = fields.Str(required=True)
+    contents = fields.Str(required=True)
+    owner_id = fields.Int(required=True)
+    created_at = fields.DateTime(dump_only=True)
+    modified_at = fields.DateTime(dump_only=True)
